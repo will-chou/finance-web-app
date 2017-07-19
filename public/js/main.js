d638c9366db982a5ded941eba6a4b9493bbdc03f
William Chou <wwchou@g.ucla.edu>
2017-07-18 17:44:48 -0700

    revert main.js merge conflicts

--- public/js/main.js
+++ public/js/main.js
@@ -121,70 +121,7 @@ function fetchDataCurrent(){
       url:   "/current?symbol=" + keyword,
       dataType: 'json',
       success: function(results){
-<<<<<<< HEAD
-
-        console.log(results);
-        var mountpoint = document.getElementById('mountpoint');
-
-
-        var StockObject = results['Realtime Global Securities Quote'];
-        console.log(StockObject);
-        var StockName = StockObject['01. Symbol'];
-        var StockPrice = StockObject['03. Latest Price'];
-        var StockPriceChange = StockObject['08. Price Change'];
-        var StockPriceChangePercent = StockObject['09. Price Change Percentage'];
-
-        console.log(StockPrice);
-        console.log(StockPriceChange);
-        console.log(StockPriceChangePercent);
-
-        var infoBox = document.createElement('div');
-        var textName = document.createElement('p');
-        var textPrice = document.createElement('p');
-        var textPriceChange = document.createElement('p');
-        var textPriceChangePercent = document.createElement('p');
-
-        infoBox.className = "infoBox";
-        textName.className = "stockName";
-        textPrice.className = "stockInfo";
-        textPriceChange.className = "stockInfo";
-        textPriceChangePercent.className = "stockInfo";
-        
-        var stockColor;
-        //change color of text
-        if(StockPriceChange < 0){
-          stockColor = "#ff6868";
-
-        }else{
-          stockColor = "#61ce81";
-
-        }
-
-        textName.innerHTML = StockName;
-        textPrice.innerHTML = "Current : $" + Math.round(StockPrice*100)/100;
-        textPriceChange.innerHTML = "Delta : $" + "<span style=color:"+stockColor+">"+Math.round(StockPriceChange*100)/100 + "</span>";
-        textPriceChangePercent.innerHTML = "Percent Change : " + "<span style=color:"+stockColor+">"+StockPriceChangePercent+"</span>";
-
-
-        var bar = document.createElement('div');
-        bar.className = "bar";
-
-
-        infoBox.appendChild(textName);
-        infoBox.appendChild(bar);
-        infoBox.appendChild(textPrice);
-        infoBox.appendChild(textPriceChange);
-        infoBox.appendChild(textPriceChangePercent);
-
-        mountpoint.appendChild(infoBox);
-
-
-
-
-        
-=======
         var display = true;
->>>>>>> 3fc6f21bf3c2f3769321a111668ac0a783039fd7
         //displayGraph(keyword);
         //ADD SYMBOL TO USER'S SHIT
         $.ajax({
@@ -201,19 +138,6 @@ function fetchDataCurrent(){
               console.log("error");
             }
         });
-
-        // $.ajax({
-        //     url: "/mongo/update/" + keyword,
-        //     type: "PUT",
-        // }).done(function(updateResults){
-        //       console.log("updateResults: " + updateResults);
-        //       if(updateResults === "Symbol already saved") {
-        //         display = false;
-        //       }
-        // }).fail(function(){
-        //       console.log("error");
-        //     }
-        // });
 
         console.log("Display boolean: " + display);
 
